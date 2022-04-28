326b2da1d8105a183ff9f01d5fb4ef0cfcbe0d18
Haur514 <u470749k@ecs.osaka-u.ac.jp>
2022-04-28 15:00:52 +0900

    test

--- .github/test.sh
+++ .github/test.sh
@@ -2,13 +2,14 @@
 export today=`date "+%Y%m%d"`
 password="2NQNWJFDQQKO60LPVWYFSCYNVG2LY4L6M2Z7RW6X9TEGSDO9Z5"
 git clone -b minutes git@github.com:Haur514/mand.git
+npm install -g @hackmd/hackmd-cli
 mkdir ~/.hackmd
 touch ~/.hackmd/config.json
 echo "{" >~/.hackmd/config.json
 echo "  \"accessToken\": \"${password}\"" >>~/.hackmd/config.json
 echo "}" >>~/.hackmd/config.json
 hackmd-cli login
-hackmd-cli export --noteId=g4vYVG5kQ3G9HoH5WNGvZg > ./mand/report.md
+hackmd-cli export --noteId=g4vYVG5kQ3G9HoH5WNGvZg
 cd mand
 git add report.md
 git commit -m "$today"
