b767ccbef8957e7616d60237531ca80501c39d5b
Haur514 <u470749k@ecs.osaka-u.ac.jp>
2022-04-28 19:38:43 +0900

    本番テスト

--- .github/test.sh
+++ .github/test.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl "https://api.hackmd.io/v1/notes/KuNSdSkcS-u0W2LM5ExSzA" -H "Authorization: Bearer 2NQNWJFDQQKO60LPVWYFSCYNVG2LY4L6M2Z7RW6X9TEGSDO9Z5" | jq -r '.content'
+curl "https://api.hackmd.io/v1/notes/wyJY9-1vQjuZTDHUNdbd9g" -H "Authorization: Bearer 2NQNWJFDQQKO60LPVWYFSCYNVG2LY4L6M2Z7RW6X9TEGSDO9Z5" | jq -r '.content' 
